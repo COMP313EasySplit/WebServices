4c3a4378c2da92da72a9e4ecab645d30b9d94baa
Divneet Sethi <divneet91@yahoo.com>
2015-04-09 17:03:18 -0400

    Summary.cs added

---
+++ EasySplitService/EasySplitService/Entities/Summary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EasySplitService.Entities
+{
+    public class Summary
+    {
+        int userId;
+        double amount;
+
+
+        public int UserId
+        {
+            get { return userId; }
+            set { userId = value; }
+        }
+      
+
+        public double Amount
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+    }
+}
